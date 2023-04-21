c6f8a56e4e5b11cb4aa6b4956ec38855b8b84b8f
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-04-21 18:15:51 -0500

    edit contact information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavBar from "./components/Navbar";
+import ListStudents from "./components/ListStudents";
+import Footer from "./components/Footer";
 
 function App() {
-
-  return (
-    <div className="App">
-      <MyNavBar />
-      <ListStudents />
-
-    </div>
-  )
+	return (
+		<div className="App">
+			<MyNavBar />
+			<ListStudents />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
