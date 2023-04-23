610eceec848a64a5e128dc79c29bb6887916f95c
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-04-22 21:23:48 -0500

    banner containers created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavBar from "./components/Navbar";
 import ListStudents from "./components/ListStudents";
 import Footer from "./components/Footer";
+import Banner from "./components/banner";
 
 function App() {
 	return (
 		<div className="App">
 			<MyNavBar />
+			<Banner />
 			<ListStudents />
 			<Footer />
 		</div>
--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,5 +1,14 @@
-const  Banner = () =>{
+const Banner = () => {
+	return (
+		<div className="banner-container">
+			<div className="banner-box-one">
+				<p>box one</p>
+			</div>
+			<div className="banner-box-two">
+				<p>box two</p>
+			</div>
+		</div>
+	);
+};
 
-}
-
-export default Banner
+export default Banner;
