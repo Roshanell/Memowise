06064c4080f37362f4b11c6d1a10089dd9d7f26e
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-04-24 14:02:13 -0500

    edited css on bannar

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,12 +1,8 @@
 const Banner = () => {
 	return (
 		<div className="banner-container">
-			<div className="banner-box-one">
-				<p>box one</p>
-			</div>
-			<div className="banner-box-two">
-				<p>box two</p>
-			</div>
+			<div className="banner-box-one"></div>
+			<div className="banner-box-two"></div>
 		</div>
 	);
 };
