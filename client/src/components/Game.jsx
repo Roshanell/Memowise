0e2225c0f64400efbb51bbf8d6f217d859c50f51
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-04-28 12:53:52 -0500

    added navigation arrows to game section

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,8 +1,9 @@
 import Button from "./Button";
+import NextCardButton from "./NextCardButton";
 const Game = () => {
 	return (
 		<div id="game-section">
-				<i class="fa fa-volume-up"></i>
+			<i class="fa fa-volume-up"></i>
 			<div className="image-container">
 				<img
 					src="https://pixabay.com/get/g0525b7442c45d2a930cae7f1ac719087d22aba7400ac293438569cf0f036b92079aa149bf6413d33154ffabc8752d8e3fbc2dbb6294d1e31ccf73bd97d3a81a4_1280.jpg"
@@ -14,6 +15,8 @@ const Game = () => {
 				<Button className="multiple-choice-button" />
 				<Button className="multiple-choice-button" />
 			</div>
+			<NextCardButton />
+			<NextCardButton />
 		</div>
 	);
 };
