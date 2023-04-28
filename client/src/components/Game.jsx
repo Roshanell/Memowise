f1813c2529b67bd118cea908f6bcaf39d7a7eb97
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-04-28 10:09:50 -0500

    volume icon added to game section

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -2,6 +2,7 @@ import Button from "./Button";
 const Game = () => {
 	return (
 		<div id="game-section">
+				<i class="fa fa-volume-up"></i>
 			<div className="image-container">
 				<img
 					src="https://pixabay.com/get/g0525b7442c45d2a930cae7f1ac719087d22aba7400ac293438569cf0f036b92079aa149bf6413d33154ffabc8752d8e3fbc2dbb6294d1e31ccf73bd97d3a81a4_1280.jpg"
