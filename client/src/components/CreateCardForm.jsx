e5bb86128a1836aa9c101723423f5ac4b20620aa
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-05-03 17:06:28 -0500

    created a form for creating card

--- client/src/components/CreateCardForm.jsx
+++ client/src/components/CreateCardForm.jsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 export default function CreateCardForm() {
-	 const [isShowing, setIsShowing] = useState(true);
 	const [card, setCard] = useState({
 		concept: "",
 		correctAnswer: "",
@@ -14,117 +12,78 @@ export default function CreateCardForm() {
 		wrongAnswerTwo: "",
 	});
 
-	// const handleClose = () => setShow(false);
-	// const handleShow = () => setShow(true);
-	const [value, setValue] = useState("");
-	const handleChange = (event) => setValue(event.target.value);
-
 	const handleInputChange = (event) => {
-		// extracts the name and value props from the input elem that is triggered to change.
 		const { name, value } = event.target;
-		// uses set card function to update the state of the card obj
-		// takes func and update the properties that have changed
-		// new state val uses spread operator to get previous data ,
-		//then stores the value of the prop that matches the name extracted  to the new value
 		setCard((prevCard) => ({ ...prevCard, [name]: value }));
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		console.log(card);
-		setIsShowing((prevVal) => !prevVal);
-
-		console.log("modal submitted");
-	};
-	const handleShowModal = (event) => {
-		// console.log("clicked");
-		setIsShowing(true);
+		// Call the API or do any other necessary action here
 	};
 
 	return (
-		<>
-			 <Button variant="outline-primary" onClick={handleShowModal}>
-				Create a Card
-			</Button> 
-			<Modal centered show={isShowing}>
-				<Modal.Header closeButton>
-					<Modal.Title>Create a Card</Modal.Title>
-				</Modal.Header>
-
-				<Modal.Body>
-					<p>Please enter your Employee ID</p>
-					<Form onSubmit={handleSubmit}>
-						<Form.Group controlId="emploeeId">
-							<Form.Label>Employee ID:</Form.Label>
-							<Form.Control
-								type="text"
-								required
-								value={value}
-								onChange={handleChange}
-							/>
-						</Form.Group>
-						<Form.Group controlId="formConcept">
-							<Form.Label>Card Concept</Form.Label>
-							<Form.Control
-								type="text"
-								name="concept"
-								value={card.concept}
-								onChange={handleInputChange}
-							/>
-						</Form.Group>
-						<Form.Group controlId="formAnswer">
-							<Form.Label>Answer</Form.Label>
-							<Form.Control
-								type="text"
-								name="correctAnswer"
-								value={card.correctAnswer}
-								onChange={handleInputChange}
-							/>
-						</Form.Group>
-						<Form.Group controlId="formImageLink">
-							<Form.Label>Image Link</Form.Label>
-							<Form.Control
-								type="text"
-								name="imageLink"
-								value={card.imageLink}
-								onChange={handleInputChange}
-							/>
-						</Form.Group>
-						<Form.Group controlId="formAudioLink">
-							<Form.Label>Audio Link</Form.Label>
-							<Form.Control
-								type="text"
-								name="audioLink"
-								value={card.audioLink}
-								onChange={handleInputChange}
-							/>
-						</Form.Group>
-						<Form.Group controlId="formWrongAnswerOne">
-							<Form.Label>Wrong Answer 1 (optional)</Form.Label>
-							<Form.Control
-								type="text"
-								name="wrongAnswerOne"
-								value={card.wrongAnswerOne}
-								onChange={handleInputChange}
-							/>
-						</Form.Group>
-						<Form.Group controlId="formWrongAnswerTwo">
-							<Form.Label>Wrong Answer 2 (optional)</Form.Label>
-							<Form.Control
-								type="text"
-								name="wrongAnswerTwo"
-								value={card.wrongAnswerTwo}
-								onChange={handleInputChange}
-							/>
-						</Form.Group>
-						<Form.Group>
-							<Modal.Footer>
-								<Button type="submit">Continue</Button>
-							</Modal.Footer>
-						</Form.Group>
-					</Form>
-				</Modal.Body>
-			</Modal>
-		</>
+		<Form onSubmit={handleSubmit}>
+			<Form.Group controlId="formConcept">
+				<Form.Label>Card Concept</Form.Label>
+				<Form.Control
+					type="text"
+					name="concept"
+					value={card.concept}
+					onChange={handleInputChange}
+					required
+				/>
+			</Form.Group>
+			<Form.Group controlId="formAnswer">
+				<Form.Label>Answer</Form.Label>
+				<Form.Control
+					type="text"
+					name="correctAnswer"
+					value={card.correctAnswer}
+					onChange={handleInputChange}
+					required
+				/>
+			</Form.Group>
+			<Form.Group controlId="formImageLink">
+				<Form.Label>Image Link</Form.Label>
+				<Form.Control
+					type="text"
+					name="imageLink"
+					value={card.imageLink}
+					onChange={handleInputChange}
+				/>
+			</Form.Group>
+			<Form.Group controlId="formAudioLink">
+				<Form.Label>Audio Link</Form.Label>
+				<Form.Control
+					type="text"
+					name="audioLink"
+					value={card.audioLink}
+					onChange={handleInputChange}
+				/>
+			</Form.Group>
+			<Form.Group controlId="formWrongAnswerOne">
+				<Form.Label>Wrong Answer 1 (optional)</Form.Label>
+				<Form.Control
+					type="text"
+					name="wrongAnswerOne"
+					value={card.wrongAnswerOne}
+					onChange={handleInputChange}
+				/>
+			</Form.Group>
+			<Form.Group controlId="formWrongAnswerTwo">
+				<Form.Label>Wrong Answer 2 (optional)</Form.Label>
+				<Form.Control
+					type="text"
+					name="wrongAnswerTwo"
+					value={card.wrongAnswerTwo}
+					onChange={handleInputChange}
+				/>
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Save Card
+			</Button>
+		</Form>
 	);
 }
