744931a99eafd6f6437ab90ba1ad6fbc28897ea9
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-05-09 14:14:40 -0500

    pushing changes to main

--- client/main.jsx
+++ client/main.jsx
@@ -7,13 +7,15 @@ const DOMAIN = import.meta.env.VITE_APP_AUTH0_DOMAIN;
 const CLIENTID = import.meta.env.VITE_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Auth0Provider
-		domain={DOMAIN}
-		clientId={CLIENTID}
-		authorizationParams={{
-			redirect_uri: window.location.origin,
-		}}
-	>
-		<App />
-	</Auth0Provider>
+	
+		<Auth0Provider
+			domain={DOMAIN}
+			clientId={CLIENTID}
+			authorizationParams={{
+				redirect_uri: window.location.origin,
+			}}
+		>
+			<App />
+		</Auth0Provider>
+
 );
