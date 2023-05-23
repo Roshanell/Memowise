e35b8e51bcdfd049c3e620050b01daa550b5fd16
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-05-23 14:56:47 -0500

    updated view cards so cards render on screen without having to search

--- client/src/Pages/ViewCards.jsx
+++ client/src/Pages/ViewCards.jsx
@@ -7,39 +7,34 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Instructions from "../components/Instructions";
 
 function ViewCards() {
-	const [cards, setCards] = useState([]); // State variable to store the cards
-	const [audio, setAudio] = useState([]); // State variable for audio (not used in this code)
-	const { user } = useAuth0(); // Destructuring the user object from Auth0
-	const [globalSearchText, setGlobalSearchText] = useState(""); // State variable for global search text
-	const [filteredCards, setFilteredCards] = useState(null); // State variable for filtered cards
+	const [cards, setCards] = useState([]);
+	const [audio, setAudio] = useState([]);
+	const { user } = useAuth0();
+	const [globalSearchText, setGlobalSearchText] = useState("");
+	const [filteredCards, setFilteredCards] = useState(null);
 	const personalizedInstructions =
 		"1. Searching Cards: Enter keywords in the search bar to find specific cards.\n2. Reviewing Cards: Click on a card to view its details and contents.";
 
 	const handleSearch = (searchText) => {
-		setGlobalSearchText(searchText); // Update the global search text
+		setGlobalSearchText(searchText);
 	};
 
 	const loadCardsData = async () => {
-		setCards(await loadCards(user)); // Load cards for admin
+		setCards(await loadCards(user));
 	};
 
 	useEffect(() => {
 		if (user) {
-			// const isAdmin = user?.email === "dteacher422@gmail.com"; // Check if the user is an admin?
-			loadCardsData(); // Call the loadCardsData function when the component mounts or when user or isAdmin changes
+			loadCardsData();
 		}
 	}, [user]);
 
-	// const newFilteredCards = cards.filter((card) => {
-	// 	console.log(card);
-	// 	Object.values(card)
-	// 		.join("")
-	// 		.toLowerCase()
-	// 		.includes(globalSearchText.toLowerCase());
-	// });
+	useEffect(() => {
+		setFilteredCards(findCards());
+	}, [globalSearchText, cards]);
 
 	const findCards = () => {
-		if (globalSearchText === "") setFilteredCards(null);
+		if (globalSearchText === "") return null;
 		return cards.filter((card) =>
 			Object.values(card)
 				.join("")
@@ -48,72 +43,36 @@ function ViewCards() {
 		);
 	};
 
-	useEffect(() => {
-		// const newFilteredCards = isAdmin
-		// 	? cards // For admin, display all cards
-		// 	: cards.filter((card) =>
-		// 			Object.values(card)
-		// 				.join("")
-		// 				.toLowerCase()
-		// 				.includes(globalSearchText.toLowerCase())
-		// 	  ); // For regular users, filter cards based on search text
-
-		setFilteredCards(findCards()); // Update the filtered cards
-	}, [globalSearchText]);
-
 	const onDelete = async (card) => {
-		console.log(card, "delete method");
-		let url = `http://localhost:8080/api/cards/${card.id}`;
-		// if (isAdmin) {
-		// 	url = `http://localhost:8080/api/admin/cards/${card.id}`; // Modify the URL for admin deletion
-		// }
 		try {
+			let url = `http://localhost:8080/api/cards/${card.id}`;
 			const response = await fetch(url, {
 				method: "DELETE",
 			});
 			if (response.ok) {
-				// const loadCardsData = async () => {
-				// 	if (isAdmin) {
-				// 		setCards(await loadCards()); // Reload cards for admin after deletion
-				// 	} else {
-				// 		setCards(await loadCards(user)); // Reload cards for regular user after deletion
-				// 	}
-				// };
-				loadCardsData(); // Call the loadCardsData function to update the cards
+				loadCardsData();
 				setFilteredCards(findCards());
 			}
 		} catch (e) {
-			console.error(e); // Log any errors that occur during deletion
+			console.error(e);
 		}
 	};
 
 	return (
-		cards && (
+		<div>
+			<Banner />
 			<div>
-				<Banner />
-				<div>
-					<Instructions personalizedInstructions={personalizedInstructions} />
-					<Searchbar onSearch={handleSearch} />
-					<ul className="card-container">
-						{filteredCards === null
-							? cards.map((card) => {
-									return (
-										<li className="card-list" key={card.id}>
-											<Card card={card} audio={audio} toDelete={onDelete} />
-										</li>
-									);
-							  })
-							: filteredCards.map((card) => {
-									return (
-										<li className="card-list" key={card.id}>
-											<Card card={card} audio={audio} toDelete={onDelete} />
-										</li>
-									);
-							  })}
-					</ul>
-				</div>
+				<Instructions personalizedInstructions={personalizedInstructions} />
+				<Searchbar onSearch={handleSearch} />
+				<ul className="card-container">
+					{(filteredCards === null ? cards : filteredCards).map((card) => (
+						<li className="card-list" key={card.id}>
+							<Card card={card} audio={audio} toDelete={onDelete} />
+						</li>
+					))}
+				</ul>
 			</div>
-		)
+		</div>
 	);
 }
 
