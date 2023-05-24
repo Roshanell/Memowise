11d67ca136da7fbaaaee88f824cc3ef704fed786
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-05-24 16:50:09 -0500

    adding drop down button

--- client/src/Pages/StudentsData.jsx
+++ client/src/Pages/StudentsData.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Instructions from '../components/Instructions'
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 function StudentsData() {
     	const personalizedInstructions = `Select a student's name to see thier data`;
   return (
 		<div>
 			<Instructions personalizedInstructions={personalizedInstructions} />{" "}
+			<DropdownButton id="dropdown-basic-button" title="Dropdown button">
+				<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+				<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+				<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+			</DropdownButton>
 		</div>
 	);
 }
