c86bc479f78e9da3cb12c2a8e216b9f3281ae16a
Kevin Hernandez <2459865+khernand@users.noreply.github.com>
2023-05-25 21:06:28 -0400

    Use GCP database in production

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -1,6 +1,18 @@
 const { Pool } = require("pg");
-const db = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
+
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+	// Use connection details for GCP
+	db = new Pool({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+	});
+} else {
+	db = new Pool({
+		connectionString: process.env.DATABASE_URL,
+	});
+}
 
 module.exports = db;
