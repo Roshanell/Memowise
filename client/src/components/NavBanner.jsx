baee05d05c824d2168175f27ec4ee4f6775f9498
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-05-26 17:36:03 -0500

    updated banner name

---
+++ client/src/components/NavBanner.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+
+// this component is strictly for css and styling
+const Banner = () => {
+	return (
+		<div>
+			<div className="banner-container">
+				<div className="banner-box-one">
+					
+				</div>
+				<div className="banner-box-two"></div>
+			</div>
+		</div>
+	);
+};
+
+//TO DO- add overlays and title to this section
+
+export default Banner;
