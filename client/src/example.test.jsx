61f5ed653143166734ec3e1f4978db0efa9d8463
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-05-28 15:24:32 -0500

    adding more tests

--- client/src/example.test.jsx
+++ client/src/example.test.jsx
@@ -1,26 +1,67 @@
 import MyNavBar from "./components/MyNavBar";
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
 import Student from "./components/Student";
-import Game from "./components/Game"
+import CreateCardForm from "./components/CreateCardForm";
+import Flashcard from "./components/Flashcard";
+import { MemoryRouter } from "react-router-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { assert, expect, test } from "vitest";
+import Footer from "./components/Footer";
+import Game from "./components/Game";
+import Searchbar from "./components/Searchbar";
 
-// test("Navbar renders correctly", () => {
-// 	const { getByTestId } = render(<MyNavBar />);
-// 	const navbarElement = getByTestId("navbar");
-// 	expect(navbarElement).toBeDefined();
-// });
-
-
-
-test("Show full name of a student", () => {
-	const student = { firstname: "Patricia", lastname: "Briggs" };
-	const { getByText } = render(<Student student={student} />);
-	const studentFullName = getByText("Patricia Briggs");
-	expect(studentFullName).toBeDefined();
+test("renders the Create Card Form component", () => {
+	render(
+		<MemoryRouter>
+			<CreateCardForm />
+		</MemoryRouter>
+	);
+	assert("Create Card Form not rendered correctly");
 });
 
+test("Footer renders correctly", () => {
+	render(
+		<MemoryRouter>
+			<Footer />
+		</MemoryRouter>
+	);
 
+	assert("Footer element not rendered correctly");
+});
 
+test("Game renders correctly", () => {
+	render(
+		<MemoryRouter>
+			<Game />
+		</MemoryRouter>
+	);
 
+	assert("Game element not rendered correctly");
+});
+test("Navbar renders correctly", () => {
+	render(
+		<MemoryRouter>
+			<MyNavBar />
+		</MemoryRouter>
+	);
+
+	// const navbarElement = screen.getByTestId("navbar");
+	assert("Navbar element not rendered correctly");
+});
+test("searchbar renders correctly", () => {
+	render(
+		<MemoryRouter>
+			<Searchbar />
+		</MemoryRouter>
+	);
+
+	// const navbarElement = screen.getByTestId("navbar");
+	assert("searchbar element not rendered correctly");
+});
+test("Show full name of a student", () => {
+	const student = { firstname: "Roshanell", lastname: "Francisco" };
+	const { getByText } = render(<Student student={student} />);
+	const studentFullName = getByText("Roshanell Francisco");
+	expect(studentFullName).toBeDefined();
+});
 
 
